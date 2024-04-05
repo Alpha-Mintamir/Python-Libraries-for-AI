3b18edf09acd155c5b27a88941e40970f6de3d80
Alpha Lencho <Alphalenchoo@gmail.com>
2024-04-06 02:04:11 +0300

    Image resize
    although using numpy is not strictly necessary, the resized picture array will be used for further edition using numpy array manipulation.

---
+++ NumPy/mini_project/image resizer.py
@@ -0,0 +1,12 @@
+import numpy as np
+from PIL import Image
+
+image = Image.open('numpy_image.png')
+
+desired_size = (30,200)
+
+resized_image = image.resize(desired_size)
+
+# photo = np.array(resized_image)
+
+resized_image.show()
