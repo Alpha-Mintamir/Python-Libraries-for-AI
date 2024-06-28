b744f73000e26a94df46eb1f15436c259a8a0508
Alpha Lencho <Alphalenchoo@gmail.com>
2024-06-28 11:19:52 +0300

    Split the data into training and testing sets

--- scikit-learn/practice two with load_boston.py
+++ scikit-learn/practice two with load_boston.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
 
 
 # Load the Boston House Prices dataset
@@ -7,4 +8,8 @@ boston = load_boston()
 # Separate features (X) and target (y)
 X = boston.data
 y = boston.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
